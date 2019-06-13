e7d539d15405ae104c8b9bcf1b5219ba94e48c49
Roman <43218195+romcikid@users.noreply.github.com>
2019-06-14 00:55:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,28 @@ $(document).ready(function($) {
 		$(this).delay(500 * index).fadeIn(10);
 	});
 
+    $('#games').click(function() { 
+        // Отображаем скрытый блок 
+        $('#invisible').fadeIn("slow"); // fadeIn - плавное появление
+    }); // end of click()
+
+    $('#finish').click(function() { 
+        // Отображаем скрытый блок 
+        $('#invisible').fadeOut("slow"); // fadeIn - плавное появление
+    }); // end of click()
+
+    $('#exe').click(function() { 
+        // Отображаем скрытый блок 
+        $('#code_file').fadeIn("slow"); // fadeIn - плавное появление
+    }); // end of click()
+
+     $('#anexe').click(function() { 
+        // Отображаем скрытый блок 
+        $('#code_file').fadeIn("slow"); // fadeIn - плавное появление
+    }); // end of click()
+
+    $('#close').click(function() { 
+        // Отображаем скрытый блок 
+        $('#code_file').fadeOut("slow"); // fadeIn - плавное появление
+    }); // end of click()
 });
